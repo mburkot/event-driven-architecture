4c92a228d1ed1b27730d64e66acfe1def47f3e4a
Matheus Burkot <m.c.burkot@gmail.com>
2024-01-16 15:09:38 -0300

    Final commit

--- OrderAPIReadonly/app.py
+++ OrderAPIReadonly/app.py
@@ -8,3 +8,8 @@ app = Flask(__name__)
 @app.route("/orders/<order_number>", methods=['GET'])
 def get_order_by_order_number(order_number):
     return order_repository.get_order_by_order_number(order_number)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
